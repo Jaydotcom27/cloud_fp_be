45f6487cd0137456292dbeafb19c31a9f512adeb
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-08-06 18:41:32 -0400

    Updated backend to work with Postgre

--- index.js
+++ index.js
@@ -1,82 +1,105 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = 3001;
+const router = express.Router();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+});
 
-const db = new sqlite3.Database('./my-database.db', (err) => {
+const pool = new Pool({
+  user: 'database',
+  host: '34.123.111.220',
+  database: 'todo_data',
+  password: 'root',
+  port: '5432',
+});
+
+pool.connect((err, client, done) => {
   if (err) {
-    console.error('Database connection error:', err.message);
+    console.error('Error connecting to the database:', err);
   } else {
-    console.log('Connected to the SQLite database.');
+    console.log('Connected to PostgreSQL database.');
   }
 });
 
+module.exports = pool;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/', router);
+
 // Get all tasks
-app.get('/api/tasks', (req, res) => {
-  db.all('SELECT * FROM tasks', (err, rows) => {
-    if (err) {
-      console.error('Error fetching tasks from the database:', err.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(rows);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM tasks');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching tasks:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Add a new task
-app.post('/api/tasks', (req, res) => {
+router.post('/api/tasks', async (req, res) => {
   const { text } = req.body;
   if (!text) {
     return res.status(400).json({ error: 'Task text is required' });
   }
 
-  db.run('INSERT INTO tasks (text) VALUES (?)', [text], function (err) {
-    if (err) {
-      console.error('Error adding task to the database:', err.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.status(201).json({ id: this.lastID, text, completed: 0 });
-    }
-  });
+  try {
+    const query = 'INSERT INTO tasks (text) VALUES ($1) RETURNING *';
+    const values = [text];
+    const result = await pool.query(query, values);
+    console.log(result);
+    const task = result.rows[0];
+    res.status(201).json({ id: task.id, text: task.text, completed: task.completed });
+  } catch (err) {
+    console.error('Error adding task to the database:', err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Update a task
-app.put('/api/tasks/:id', (req, res) => {
+router.put('/api/tasks/:id', async (req, res) => {
   const { id } = req.params;
   const { text, completed } = req.body;
   if (!text) {
     return res.status(400).json({ error: 'Task text is required' });
   }
 
-  db.run('UPDATE tasks SET text = ?, completed = ? WHERE id = ?', [text, completed, id], function (err) {
-    if (err) {
-      console.error('Error updating task in the database:', err.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json({ id: Number(id), text, completed });
-    }
-  });
+  try {
+    const query = 'UPDATE tasks SET text = $1, completed = $2 WHERE id = $3 RETURNING *';
+    const values = [text, completed, id];
+    const result = await pool.query(query, values);
+    const task = result.rows[0];
+    res.json({ id: task.id, text: task.text, completed: task.completed });
+  } catch (err) {
+    console.error('Error updating task in the database:', err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Delete a task
-app.delete('/api/tasks/:id', (req, res) => {
+router.delete('/api/tasks/:id', async (req, res) => {
   const { id } = req.params;
 
-  db.run('DELETE FROM tasks WHERE id = ?', [id], function (err) {
-    if (err) {
-      console.error('Error deleting task from the database:', err.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json({ message: 'Task deleted successfully' });
-    }
-  });
+  try {
+    const query = 'DELETE FROM tasks WHERE id = $1';
+    const values = [id];
+    await pool.query(query, values);
+    res.json({ message: 'Task deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting task from the database:', err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Backend server is running on http://localhost:${port}`);
